94f81bb3959d878e998700aaf289933df837c3dc
“eduardevs <jeseduardopi@gmail.com>
2024-02-21 01:02:07 +0100

    Add the hexagonal architecture folders structure

--- src/test/java/fr/sportpourtous/Main.java
+++ src/test/java/fr/sportpourtous/Main.java
@@ -1,4 +1,4 @@
-package com.sportpourtous;
+package fr.sportpourtous;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
